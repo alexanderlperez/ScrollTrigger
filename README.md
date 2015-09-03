ccb7f383a2e1b6dbf65f6fe58422b0272213f481
Alex <alexanderlperez@gmail.com>
2015-09-02 22:47:57 -0400

    Added files and basic README

---
+++ README.md
@@ -0,0 +1,16 @@
+# ScrollTrigger
+### Something for going up, something for going down
+
+A simple JS plugin that calls whatever you like when a user's screen hits a position, based on the position of the vertical scroll bar.
+
+#### Usage: 
+
+``` javascript
+Scroll_Trigger({
+    triggerPosition: 1, // this can be set with $(element).offset() too
+    triggerDownCallback: sticky_scroll_down, // triggers when scrolling downwards past trigger position
+    triggerUpCallback: sticky_scroll_up // triggers when scrolling upwards past trigger position
+});
+```
+
+
---
+++ scrolltrigger.js
@@ -0,0 +1,51 @@
+// Depends on jQuery
+
+// Runs trigger code once per direction 
+// Handles effects depending what direction
+var Scroll_Trigger = function (options) {
+    var $window = $(window);
+
+    var triggerDownCallback = options ? options.triggerDownCallback : function () {};
+    var triggerUpCallback = options ? options.triggerUpCallback : function () {};
+    var triggerPos = options ? options.triggerPosition : 0;
+
+    var lastTriggerDirection = 'up'; 
+    var lastPos = $window.scrollTop();
+
+    function getDir() {
+        return $window.scrollTop() >= lastPos ? 'down' : 'up';
+    }
+
+    function isTriggered() {
+        if (getDir() === 'down') {
+            return $window.scrollTop() > triggerPos;
+        } else if (getDir() === 'up') {
+            return $window.scrollTop() < triggerPos;
+        }
+    }
+
+    $window.scroll(function () {
+        if (isTriggered()) {
+            // only run the code once per trigger direction
+
+            switch (getDir()) {
+                case 'down': 
+                    if (lastTriggerDirection === 'up') {
+                        triggerDownCallback();
+                        lastTriggerDirection = 'down';
+                    }
+                    break;
+
+                case 'up':
+                    if (lastTriggerDirection === 'down') {
+                        triggerUpCallback();
+                        lastTriggerDirection = 'up';
+                    }
+                    break;
+            }
+
+        }
+
+        lastPos = $window.scrollTop();
+    });
+}
